2cc7ba12eea7f535ec87aeec749e01d862647379
Justus <justuskasyoki57@gmail.com>
2023-01-02 12:28:53 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphQL_Dotnet
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var schema = Schema.For(@"
+      type Query {
+        hello: String
+      }
+    ");
+
+            var json = await schema.ExecuteAsync(_ =>
+            {
+                _.Query = "{ hello }";
+                _.Root = new { Hello = "Hello World!" };
+            });
+
+            Console.WriteLine(json);
+        }
+    }
+}
